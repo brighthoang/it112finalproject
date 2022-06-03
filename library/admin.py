8faad714fff261cfa28726cc3c2143e3a5539494
brighthoang <brihoa4002@gmail.com>
2022-06-03 16:48:21 -0700

    models commit

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Books, Movies, Music
 
-# Register your models here.
+admin.site.register(Books)
+admin.site.register(Movies)
+admin.site.register(Music)
--- library/models.py
+++ library/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.forms import CharField, DateField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Books(models.Model):
+    book_title=models.CharField(max_length=255)
+    author=models.CharField(max_length=255)
+    ISBN=models.CharField(max_length=40)
+    publishyear=models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.book_title
+
+    class Meta:
+        db_table='books'
+
+
+class Movies(models.Model):
+    movie_title=models.CharField(max_length=255)
+    director=models.CharField(max_length=255)
+    genre=models.CharField(max_length=255)
+    releaseyear=models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.movie_title
+
+    class Meta:
+        db_table='movies'
+
+
+class Music(models.Model):
+    song_title=models.CharField(max_length=255)
+    artist=models.CharField(max_length=255)
+    genre=models.CharField(max_length=255)
+    releaseyear=models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.song_title
+
+    class Meta:
+        db_table='music'
+
