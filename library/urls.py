d13b42a29f4922e4794ba25b6c381a44d2687683
brighthoang <brihoa4002@gmail.com>
2022-06-03 17:53:15 -0700

    detail commit

--- library/urls.py
+++ library/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('books/', views.books, name="books"),
     path('movies/', views.movies, name="movies"),
     path('music/', views.music, name="music"),
+    path('booksdetail/<int:id>', views.booksDetail, name="booksdetail"),
+    path('moviesdetail/<int:id>', views.moviesDetail, name="moviesdetail"),
+    path('musicdetail/<int:id>', views.musicDetail, name="musicdetail"),
 ] 
--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Books, Movies, Music
+from django.urls import reverse_lazy
 
 def index(request):
     return render(request, 'library/index.html')
@@ -18,3 +19,18 @@ def movies(request):
 def music(request):
     music_list = Music.objects.all()
     return render(request, 'library/music.html', {'music_list': music_list})
+
+
+def booksDetail(request, id):
+    books=get_object_or_404(Books, pk=id)
+    return render(request, 'library/booksdetail.html', {'books': books})
+
+
+def moviesDetail(request, id):
+    movies=get_object_or_404(Movies, pk=id)
+    return render(request, 'library/moviesdetail.html', {'movies': movies})
+
+
+def musicDetail(request, id):
+    music=get_object_or_404(Music, pk=id)
+    return render(request, 'library/musicdetail.html', {'music': music})
