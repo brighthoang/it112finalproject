25a5850a6bca2d383dc1fc36e148e70e7ccedb4a
brighthoang <brihoa4002@gmail.com>
2022-06-03 17:18:32 -0700

    all 3 views commit

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.books, name="books"),
+    path('movies/', views.movies, name="movies"),
+    path('music/', views.music, name="music"),
 ] 
--- library/views.py
+++ library/views.py
@@ -1,4 +1,20 @@
+import re
 from django.shortcuts import render
+from .models import Books, Movies, Music
 
 def index(request):
     return render(request, 'library/index.html')
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, 'library/books.html', {'books_list': books_list})
+
+
+def movies(request):
+    movies_list = Movies.objects.all()
+    return render(request, 'library/movies.html', {'movies_list': movies_list})
+
+
+def music(request):
+    music_list = Music.objects.all()
+    return render(request, 'library/music.html', {'music_list': music_list})
